6e64c4cbd6d9a61350c8e05522edbbd0dba0b48c
leo3b3b <leonardo.oliveira1b3b2024@gmail.com>
2026-09-10 16:55:41 -0300

    set up React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import 'virtual:uno.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+// Components
+import App from './App.tsx';
+
+// CSS
+import './index.css';
+import 'virtual:uno.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
