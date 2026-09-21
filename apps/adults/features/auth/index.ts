8d8496598b455cd08e7305dda9762e45d97232f3
leo3b3b <leonardo.oliveira1b3b2024@gmail.com>
2026-09-21 18:26:38 -0300

    rename page files for consistency

--- apps/adults/features/auth/index.ts
+++ apps/adults/features/auth/index.ts
@@ -1,5 +1,5 @@
 export { default as AuthLayout } from "./auth.layout";
 export { requireAuth } from "./auth.service";
-export { default as CheckEmailPage } from "./CheckEmail";
-export { default as SignInPage } from "./SignIn";
-export { default as SignUpPage } from "./SignUp";
+export { default as CheckEmailPage } from "./CheckEmailPage";
+export { default as SignInPage } from "./SignInPage";
+export { default as SignUpPage } from "./SignUpPage";
